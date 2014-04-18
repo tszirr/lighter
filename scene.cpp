b0ac50c267589b2d507c5e92dc6fef62da35e64b
tszirr <none@none>
2014-04-19 00:30:29 +0200

    add: basic scene data & io

---
+++ scene.cpp
@@ -0,0 +1,42 @@
+#include "scene"
+
+#include <algorithm>
+
+namespace scene
+{
+
+unsigned DataHeader::make_id(char const* id)
+{
+	union
+	{
+		unsigned n;
+		char c[4];
+	} did;
+	
+	assert (id != nullptr);
+	
+	did.n = 0;
+	for (int i = 0; auto c = id[i]; ++i)
+	{
+		did.c[i] = c;
+		assert (i < arraylen(did.c));
+	}
+
+	return did.n;
+}
+
+DataHeader DataHeader::make(char const* id, size_t size, unsigned version, size_t elementSize)
+{
+	DataHeader header;
+
+	header.id = make_id(id);
+	header.version = version;
+
+	assert (elementSize <= size && size <= ~0U);
+	header.elementSize = unsigned(elementSize);
+	header.size = unsigned(size);
+
+	return header;
+}
+
+} // namespace
