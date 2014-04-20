f11485b551bd9ab673a3b84bc45a29f1270de3a4
tszirr <none@none>
2014-04-20 02:09:25 +0200

    chg: omit empty data chunks fix: work with count rather than size

--- scene.cpp
+++ scene.cpp
@@ -25,17 +25,17 @@ unsigned DataHeader::make_id(char const* id)
 	return did.n;
 }
 
-DataHeader DataHeader::make(char const* id, size_t size, unsigned version, size_t elementSize)
+DataHeader DataHeader::make(char const* id, size_t count, unsigned version, size_t elementSize)
 {
 	DataHeader header;
 
 	header.id = make_id(id);
 	header.version = version;
 
-	assert (elementSize <= ~0U && size <= ~0U);
-	assert (elementSize > 0 && size % elementSize == 0);
+	assert (elementSize > 0);
+	assert (elementSize * count <= ~0U);
 	header.elementSize = unsigned(elementSize);
-	header.size = unsigned(size);
+	header.size = unsigned(elementSize * count);
 
 	return header;
 }
