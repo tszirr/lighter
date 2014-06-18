9e1ec873c13ff52a376db6d8d1826eb6cba1d45f
tszirr <none@none>
2014-06-18 19:32:24 +0200

    add: built-in preset functionality w/ user interface add: system dialog prompts
    --HG--
    rename : ui.cpp => uii.cpp
    extra : rebase_source : 1f6592bdd61bb724bc43b9eca9a5587df583da55

--- ui.cpp
+++ ui.cpp
@@ -1,6 +1,9 @@
 #include "uix"
 #include "textx"
 
+#include "file"
+#include "appx"
+
 namespace ui
 {
 
@@ -66,222 +69,34 @@ std::unique_ptr<UserInterface> UserInterface::create(text::FreeTypeRef* freeType
 	return std::unique_ptr<UserInterface>(new DefaultUserInterface(freeTypeLib, font, textProgram, widgetProgram));
 }
 
-} // namespace
-
-#include <cstdio>
-
-namespace ui
+void preset_user_interface(UniversalInterface& ui, stdx::fun_ref<void(UniversalInterface&)> target, char const* defaultFile)
 {
-
-struct UiToText : UniversalInterface
-{
-	typedef std::pair<std::string, std::string> tuple;
-	
-	KeyValueStream* stream;
-
-	std::string groupPrefix;
-	std::vector<size_t> groupPrefixLen;
-	size_t groupLabelPending;
-
-	UiToText(KeyValueStream& stream)
-		: stream(&stream)
-		, groupPrefixLen(1, 0)
-		, groupLabelPending(0) { }
-
-	bool pushGroup(UniqueElementIdentifier id, bool openByDefault = true) override
+	if (auto presetGroup = ui::Group(ui, nullptr))
 	{
-		++groupLabelPending;
-		return true;
-	}
-	void popGroup(UniqueElementIdentifier id) override
-	{
-		if (groupLabelPending > 0)
-			--groupLabelPending;
-		else
+		uintptr_t controlIdentifier = 0;
+
+		ui.addText(controlIdentifier++, "Presets", "", nullptr);
+
+		ui.addButton(controlIdentifier++, "save", [&]()
 		{
-			stream->leaveSection();
-			groupPrefixLen.pop_back();
-			assert (!groupPrefixLen.empty()); // Always keep 0 sentinel
-			groupPrefix.resize(groupPrefixLen.back());
-		}
-	}
-	
-	char const* keyFromLabel(char const* label)
-	{
-		if (groupLabelPending > 0)
-		{
-			if (!groupPrefix.empty())
-				groupPrefix.push_back('.');
-			groupPrefix.append(label);
-			groupPrefixLen.push_back(groupPrefix.size());
-			--groupLabelPending;
-			stream->enterSection(label, groupPrefix.c_str());
-			return "$";
-		}
-		else
-			return label;
-	}
-	void addLabel(char const* label) override
-	{
-		// Labels are not stored, but may name groups
-		keyFromLabel(label);
-	}
-
-	void beginUnion() override
-	{
-	}
-
-	void endUnion() override
-	{
-	}
-
-	void addText(UniqueElementIdentifier id, char const* label, char const* text, InteractionParam<char const*> interact) override
-	{
-		auto key = keyFromLabel(label);
-		if (interact)
-			stream->addItem(key, text);
-	}
-
-	void addHidden(UniqueElementIdentifier id, char const* label, char const* text, InteractionParam<char const*> interact) override
-	{
-		stream->addItem(label, text);
-	}
-
-	void addInteractiveButton(UniqueElementIdentifier id, char const* text, bool value, InteractionParam<ButtonEvent::T> interact, InteractionParam<Button> control = nullptr) override
-	{
-	}
-
-	void addButton(UniqueElementIdentifier id, char const* text, InteractionParam<void> interact, char const* fullText = nullptr, InteractionParam<Button> control = nullptr) override
-	{
-//		auto key = keyFromLabel(label);
-//		(fullText) ? fullText : text;
-	}
-
-	void addOption(UniqueElementIdentifier id, char const* text, bool value, InteractionParam<bool> interact, InteractionParam<Button> control = nullptr) override
-	{
-		auto key = keyFromLabel(text);
-		if (interact)
-			stream->addItem(key, (value) ? "true" : "false");
-	}
-
-	void addSlider(UniqueElementIdentifier id, char const* label, float value, float range, InteractionParam<float> interact, float ticks = 0.0f, InteractionParam<Slider> control = nullptr) override
-	{
-		auto key = keyFromLabel(label);
-		if (interact)
-		{
-			char buf[1024];
-			sprintf(buf, "%f", value);
-			stream->addItem(key, buf);
-		}
-	}
-};
-
-void write(KeyValueStream& out, stdx::fun_ref<void(UniversalInterface&)> ui)
-{
-	UiToText uitt(out);
-	ui.dispatch(ui, uitt);
-}
-
-struct TextToUi : UniversalInterface
-{
-	typedef std::pair<std::string, std::string> tuple;
-	
-	KeyValueStore* stream;
-
-	size_t groupLabelPending;
-
-	TextToUi(KeyValueStore& stream)
-		: stream(&stream)
-		, groupLabelPending(0) { }
-
-	bool pushGroup(UniqueElementIdentifier id, bool openByDefault = true) override
-	{
-		++groupLabelPending;
-		return true;
-	}
-	void popGroup(UniqueElementIdentifier id) override
-	{
-		if (groupLabelPending > 0)
-			--groupLabelPending;
-		else
-			stream->leaveSection();
-	}
-	
-	char const* keyFromLabel(char const* label)
-	{
-		if (groupLabelPending > 0)
-		{
-			stream->enterSection(label);
-			--groupLabelPending;
-			return "$";
-		}
-		else
-			return label;
-	}
-	void addLabel(char const* label) override
-	{
-		// Labels are not stored, but may name groups
-		keyFromLabel(label);
-	}
-
-	void beginUnion() override
-	{
-	}
-
-	void endUnion() override
-	{
-	}
-
-	void addText(UniqueElementIdentifier id, char const* label, char const* text, InteractionParam<char const*> interact) override
-	{
-		auto key = keyFromLabel(label);
-		if (interact)
-			if (auto val = stream->getValue(key))
-				interact->updateValue(val);
-	}
-
-	void addHidden(UniqueElementIdentifier id, char const* label, char const* text, InteractionParam<char const*> interact) override
-	{
-		if (interact)
-			if (auto val = stream->getValue(label))
-				interact->updateValue(val);
-	}
-
-	void addInteractiveButton(UniqueElementIdentifier id, char const* text, bool value, InteractionParam<ButtonEvent::T> interact, InteractionParam<Button> control = nullptr) override
-	{
-	}
-
-	void addButton(UniqueElementIdentifier id, char const* text, InteractionParam<void> interact, char const* fullText = nullptr, InteractionParam<Button> control = nullptr) override
-	{
-//		auto key = keyFromLabel(label);
-//		(fullText) ? fullText : text;
-	}
-
-	void addOption(UniqueElementIdentifier id, char const* text, bool value, InteractionParam<bool> interact, InteractionParam<Button> control = nullptr) override
-	{
-		auto key = keyFromLabel(text);
-		if (interact)
-			if (auto val = stream->getValue(key))
-				interact->updateValue( !stdx::strieq(val, "false") && !(val[0] == '0' && val[1] == 0) );
-	}
-
-	void addSlider(UniqueElementIdentifier id, char const* label, float value, float range, InteractionParam<float> interact, float ticks = 0.0f, InteractionParam<Slider> control = nullptr) override
-	{
-		auto key = keyFromLabel(label);
-		if (interact)
-			if (auto val = stream->getValue(key))
+			auto mf = stdx::prompt_file_compat(defaultFile, "Ini files=*.ini|All files=*.*", stdx::dialog::save);
+			for (auto&&f : mf)
 			{
-				float newVal = value;
-				if (sscanf(val, "%f", &newVal) == 1)
-					interact->updateValue(newVal);
+				try { save_ini_file(f.c_str(), target); }
+				catch(...) { stdx::prompt(appx::exception_string().c_str(), "Error saving preset"); }
 			}
-	}
-};
+		});
 
-void read(KeyValueStore& in, stdx::fun_ref<void(UniversalInterface&)> ui)
-{
-	TextToUi ttui(in);
-	ui.dispatch(ui, ttui);
+		ui.addButton(controlIdentifier++, "load", [&]()
+		{
+			auto mf = stdx::prompt_file_compat(defaultFile, "Ini files=*.ini|All files=*.*", stdx::dialog::open, true);
+			for (auto&&f : mf)
+			{
+				try { load_ini_file(f.c_str(), target); }
+				catch(...) { stdx::prompt(appx::exception_string().c_str(), "Error saving preset"); }
+			}
+		});
+	}
 }
 
 } // namespace
