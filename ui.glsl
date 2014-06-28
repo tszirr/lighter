d559643dcae254281aa6ff2a5168dc41df9a9d7c
tszirr <none@none>
2014-06-29 01:02:14 +0200

    chg: ui outline
    --HG--
    extra : rebase_source : 8b49b96a7b230f2c556e3347ff0d75c6a04303eb

--- ui.glsl
+++ ui.glsl
@@ -30,28 +30,65 @@ out vec4 widgetPxF;
 
 void main()
 {
-	ivec2 leftTopPx = ivec2(int(widgetData[0].x) & 0xffff, int(widgetData[0].x) >> 16);
-	ivec2 sizePx = ivec2(int(widgetData[0].y) & 0xffff, int(widgetData[0].y) >> 16);
-	ivec2 bottomRightPx = leftTopPx + sizePx;
-
-	vec2 leftTop = vec2(leftTopPx) * camera.PixelWidth * vec2(2.0f, -2.0f) - vec2(1.0f, -1.0f);
-	vec2 bottomRight = vec2(bottomRightPx) * camera.PixelWidth * vec2(2.0f, -2.0f) - vec2(1.0f, -1.0f);
-
 	widgetClassEx = widgetData[0].zw;
 	
-	gl_Position = vec4(leftTop, 0.0, 1.0);
-	widgetPxF = vec4(0.0f, 0.0f, vec2(sizePx));
-	EmitVertex();
-	gl_Position = vec4(bottomRight.x, leftTop.y, 0.0, 1.0);
-	widgetPxF = vec4(float(sizePx.x), 0.0f, 0.0f, float(sizePx.y));
-	EmitVertex();
-	gl_Position = vec4(leftTop.x, bottomRight.y, 0.0, 1.0);
-	widgetPxF = vec4(0.0f, float(sizePx.y), float(sizePx.x), 0.0f);
-	EmitVertex();
-	gl_Position = vec4(bottomRight, 0.0, 1.0);
-	widgetPxF = vec4(vec2(sizePx), 0.0f, 0.0f);
-	EmitVertex();
-	EndPrimitive();
+	ivec2 coords1 = ivec2(int(widgetData[0].x) & 0xffff, int(widgetData[0].x) >> 16);
+	ivec2 coords2 = ivec2(int(widgetData[0].y) & 0xffff, int(widgetData[0].y) >> 16);
+
+	int lineWidthPx = int(widgetClassEx.x >> 24);
+
+	if (lineWidthPx == 0)
+	{
+		ivec2 leftTopPx = coords1;
+		ivec2 sizePx = coords2;
+		ivec2 bottomRightPx = leftTopPx + sizePx;
+
+		vec2 leftTop = vec2(leftTopPx) * camera.PixelWidth * vec2(2.0f, -2.0f) - vec2(1.0f, -1.0f);
+		vec2 bottomRight = vec2(bottomRightPx) * camera.PixelWidth * vec2(2.0f, -2.0f) - vec2(1.0f, -1.0f);
+
+		gl_Position = vec4(leftTop, 0.0, 1.0);
+		widgetPxF = vec4(0.0f, 0.0f, vec2(sizePx));
+		EmitVertex();
+		gl_Position = vec4(bottomRight.x, leftTop.y, 0.0, 1.0);
+		widgetPxF = vec4(float(sizePx.x), 0.0f, 0.0f, float(sizePx.y));
+		EmitVertex();
+		gl_Position = vec4(leftTop.x, bottomRight.y, 0.0, 1.0);
+		widgetPxF = vec4(0.0f, float(sizePx.y), float(sizePx.x), 0.0f);
+		EmitVertex();
+		gl_Position = vec4(bottomRight, 0.0, 1.0);
+		widgetPxF = vec4(vec2(sizePx), 0.0f, 0.0f);
+		EmitVertex();
+		EndPrimitive();
+	}
+	else
+	{
+		ivec2 leftTopPx = coords1;
+		ivec2 bottomRightPx = coords2;
+
+		vec2 leftTop = vec2(leftTopPx) * camera.PixelWidth * vec2(2.0f, -2.0f) - vec2(1.0f, -1.0f);
+		vec2 bottomRight = vec2(bottomRightPx) * camera.PixelWidth * vec2(2.0f, -2.0f) - vec2(1.0f, -1.0f);
+
+		vec2 dir = normalize(bottomRight - leftTop);
+		vec2 dirOrtho = vec2(-dir.y, dir.x);
+
+		float lengthPx = distance(vec2(leftTopPx), vec2(bottomRightPx));
+		float radiusPx = float(lineWidthPx) * 0.5f;
+		vec2 radiusOrtho = dirOrtho * radiusPx * camera.PixelWidth * 2.0f;
+
+		gl_Position = vec4(leftTop - radiusOrtho, 0.0, 1.0);
+		widgetPxF = vec4(-radiusPx, 0.0f, radiusPx, lengthPx);
+		EmitVertex();
+		gl_Position = vec4(leftTop + radiusOrtho, 0.0, 1.0);
+		widgetPxF = vec4(radiusPx, 0.0f, -radiusPx, lengthPx);
+		EmitVertex();
+		gl_Position = vec4(bottomRight - radiusOrtho, 0.0, 1.0);
+		widgetPxF = vec4(-radiusPx, lengthPx, radiusPx, 0.0f);
+		EmitVertex();
+		gl_Position = vec4(bottomRight + radiusOrtho, 0.0, 1.0);
+		widgetPxF = vec4(radiusPx, lengthPx, -radiusPx, 0.0f);
+		EmitVertex();
+		EndPrimitive();
+	}
 }
 
 #endif
@@ -70,15 +107,24 @@ void main()
 	float borderDist = min4(widgetPxF);
 	vec4 color = vec4(1.0f, 0.0f, 1.0f, 1.0f);
 	// Bar
-	if (widgetClassEx.x == 1U)
+	if ((widgetClassEx.x & 0xffU) == 1U)
 		color.xyz = vec3(0.2f);
 	// Ticks
-	else if (widgetClassEx.x == 2U)
+	else if ((widgetClassEx.x & 0xffU) == 2U)
 	{
 		color.xyz = vec3(0.0f);
 		float tickDelta = uintBitsToFloat(widgetClassEx.y);
 		float tickDist = abs(mod(widgetPxF.x + 0.5f * tickDelta, tickDelta) - 0.5f * tickDelta);
 		color.w = (tickDist <= 0.5f) ? 0.9f : 0.0f; //  || widgetPxF.w <= 1.0f && 2.5f <= tickDist && tickDist < 3.5f
+	}
+	// Lines
+	else if ((widgetClassEx.x & 0xffU) == 3U)
+		color.xyz = vec3(0.2f);
+	else if ((widgetClassEx.x & 0xffU) == 4U)
+	{
+		color.xyz = vec3(0.2f);
+		color.w = fract(gl_FragCoord.y / 6.0f) > 0.5f ? 0.5f : 1.0f;
+		color.xyz *= color.w;
 	}
 	// Frames
 	else
