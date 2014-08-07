8930b2369e44b0b94f9f7f21907e1183ac04fde2
tszirr <none@none>
2014-08-07 17:39:04 +0200

    add: color picker control drawing add: primitive slider-based color selection todo: integrate picker controls

--- ui.glsl
+++ ui.glsl
@@ -27,6 +27,7 @@ layout(triangle_strip, max_vertices = 4) out;
 flat in uvec4 widgetData[1];
 flat out uvec2 widgetClassEx;
 out vec4 widgetPxF;
+out vec2 widgetF;
 
 void main()
 {
@@ -48,15 +49,19 @@ void main()
 
 		gl_Position = vec4(leftTop, 0.0, 1.0);
 		widgetPxF = vec4(0.0f, 0.0f, vec2(sizePx));
+		widgetF = vec2(0.0f);
 		EmitVertex();
 		gl_Position = vec4(bottomRight.x, leftTop.y, 0.0, 1.0);
 		widgetPxF = vec4(float(sizePx.x), 0.0f, 0.0f, float(sizePx.y));
+		widgetF = vec2(1.0f, 0.0f);
 		EmitVertex();
 		gl_Position = vec4(leftTop.x, bottomRight.y, 0.0, 1.0);
 		widgetPxF = vec4(0.0f, float(sizePx.y), float(sizePx.x), 0.0f);
+		widgetF = vec2(0.0f, 1.0f);
 		EmitVertex();
 		gl_Position = vec4(bottomRight, 0.0, 1.0);
 		widgetPxF = vec4(vec2(sizePx), 0.0f, 0.0f);
+		widgetF = vec2(1.0f, 1.0f);
 		EmitVertex();
 		EndPrimitive();
 	}
@@ -77,15 +82,19 @@ void main()
 
 		gl_Position = vec4(leftTop - radiusOrtho, 0.0, 1.0);
 		widgetPxF = vec4(-radiusPx, 0.0f, radiusPx, lengthPx);
+		widgetF = vec2(0.0f);
 		EmitVertex();
 		gl_Position = vec4(leftTop + radiusOrtho, 0.0, 1.0);
 		widgetPxF = vec4(radiusPx, 0.0f, -radiusPx, lengthPx);
+		widgetF = vec2(1.0f, 0.0f);
 		EmitVertex();
 		gl_Position = vec4(bottomRight - radiusOrtho, 0.0, 1.0);
 		widgetPxF = vec4(-radiusPx, lengthPx, radiusPx, 0.0f);
+		widgetF = vec2(0.0f, 1.0f);
 		EmitVertex();
 		gl_Position = vec4(bottomRight + radiusOrtho, 0.0, 1.0);
 		widgetPxF = vec4(radiusPx, lengthPx, -radiusPx, 0.0f);
+		widgetF = vec2(1.0f, 1.0f);
 		EmitVertex();
 		EndPrimitive();
 	}
@@ -97,20 +106,43 @@ void main()
 
 flat in uvec2 widgetClassEx;
 in vec4 widgetPxF;
+in vec2 widgetF;
 
 layout(location = 0) out vec4 colorOut;
 
 float min4(vec4 v) { return min(min(v.x, v.y), min(v.z, v.w)); }
 
+vec3 fromHSV(float hue, float saturation, float value)
+{
+	float hue6 = hue * 6.0f;
+	float minValue = 1.0f - saturation;
+
+	vec3 color;
+	color.r = mix(minValue, 1.0f, clamp(2.0f - abs(hue6 < 3.0f ? hue6 : hue6 - 6.0f), 0.0f, 1.0f));
+	color.g = mix(minValue, 1.0f, clamp(2.0f - abs(hue6 - 2.0f), 0.0f, 1.0f));
+	color.b = mix(minValue, 1.0f, clamp(2.0f - abs(hue6 - 4.0f), 0.0f, 1.0f));
+
+	// "Linear HSV"
+	color *= color;
+
+	color *= value;
+
+	return color;
+}
+
 void main()
 {
+	float barGray = 0.2f;
+	float borderGray = 0.1f;
+
 	float borderDist = min4(widgetPxF);
 	vec4 color = vec4(1.0f, 0.0f, 1.0f, 1.0f);
+	uint classID = widgetClassEx.x & 0xffU;
 	// Bar
-	if ((widgetClassEx.x & 0xffU) == 1U)
-		color.xyz = vec3(0.2f);
+	if (classID == 1U)
+		color.xyz = vec3(barGray);
 	// Ticks
-	else if ((widgetClassEx.x & 0xffU) == 2U)
+	else if (classID == 2U)
 	{
 		color.xyz = vec3(0.0f);
 		float tickDelta = uintBitsToFloat(widgetClassEx.y);
@@ -118,17 +150,49 @@ void main()
 		color.w = (tickDist <= 0.5f) ? 0.9f : 0.0f; //  || widgetPxF.w <= 1.0f && 2.5f <= tickDist && tickDist < 3.5f
 	}
 	// Lines
-	else if ((widgetClassEx.x & 0xffU) == 3U)
-		color.xyz = vec3(0.2f);
-	else if ((widgetClassEx.x & 0xffU) == 4U)
+	else if (classID == 3U)
+		color.xyz = vec3(barGray);
+	else if (classID == 4U)
 	{
-		color.xyz = vec3(0.2f);
+		color.xyz = vec3(barGray);
 		color.w = fract(gl_FragCoord.y / 6.0f) > 0.5f ? 0.5f : 1.0f;
 		color.xyz *= color.w;
 	}
 	// Frames
 	else
-		color.xyz = (borderDist <= 1.0f) ? vec3(0.1f) : vec3(1.0f);
+	{
+		if (borderDist <= 1.0f)
+			color.xyz = vec3(borderGray);
+		else
+		{
+			// Hue
+			if (classID == 5U)
+			{
+				float hue = widgetF.x + uintBitsToFloat(widgetClassEx.y);
+				float value = float((widgetClassEx.x >> 16U) & 0xffU) / 255.0f;
+				float saturation = float((widgetClassEx.x >> 8U) & 0xffU) / 255.0f;
+				color.rgb = fromHSV(hue, saturation, value);
+			}
+			// Saturation
+			else if (classID == 6U)
+			{
+				float hue = uintBitsToFloat(widgetClassEx.y);
+				float value = float((widgetClassEx.x >> 16U) & 0xffU) / 255.0f;
+				float saturation = widgetF.x;
+				color.rgb = fromHSV(hue, saturation, value);
+			}
+			// Value
+			else if (classID == 7U)
+			{
+				float hue = uintBitsToFloat(widgetClassEx.y);
+				float value = widgetF.x;
+				float saturation = float((widgetClassEx.x >> 8U) & 0xffU) / 255.0f;
+				color.rgb = fromHSV(hue, saturation, value);
+			}
+			else
+				color.xyz = vec3(1.0f);
+		}
+	}
 	colorOut = color;
 }
 
