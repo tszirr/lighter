94d651c703af5fd44e41f13e7341022f4646feda
tszirr <none@none>
2015-12-07 16:23:08 +0100

    fix: ambiguity

---
+++ types.clsl.h
@@ -0,0 +1,15 @@
+using clsl::float2;
+using clsl::float3;
+using clsl::float4;
+
+using clsl::int2;
+using clsl::int3;
+using clsl::int4;
+
+using clsl::uint2;
+using clsl::uint3;
+using clsl::uint4;
+
+using clsl::bool2;
+using clsl::bool3;
+using clsl::bool4;
