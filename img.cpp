9c14fcbddc9633dcf8f9aeab6f9c1e7fcb5bded0
tszirr <none@none>
2015-12-16 15:42:08 +0100

    add: unfinished img save

--- img.cpp
+++ img.cpp
@@ -41,9 +41,9 @@ template <class T> struct copy_channel_identity::is_identity { static bool const
 template <> struct copy_channel_identity::is_identity<copy_channel_identity> { static bool const value = true; };
 
 template <class D, class S, class Transform>
-inline void copy_channels(D* destBytes, unsigned destChannels, S const* srcBytes, unsigned srcChannels, unsigned srcPitch, glm::uvec2 dim, D zero, D one, Transform&& transform)
+inline void copy_channels(D* destBytes, unsigned destChannels, S const* srcBytes, unsigned srcChannels, unsigned srcPitch, glm::uvec2 dim, D zero, D one, Transform&& transform, unsigned destPitch = 0)
 {
-	auto destByte = destBytes;
+	auto destLine = destBytes;
 	auto srcLine = srcBytes;
 	auto copyChannels = stdx::min_value(destChannels, srcChannels);
 	auto skipChannels = srcChannels - copyChannels;
@@ -51,6 +51,7 @@ inline void copy_channels(D* destBytes, unsigned destChannels, S const* srcBytes
 	for (unsigned y = 0; y < dim.y; ++y)
 	{
 		auto srcByte = srcLine;
+		auto destByte = destLine;
 
 		for (unsigned x = 0; x < dim.x; ++x)
 		{
@@ -69,11 +70,78 @@ inline void copy_channels(D* destBytes, unsigned destChannels, S const* srcBytes
 		}
 
 		srcLine = (S*) ((char*) srcLine + srcPitch);
+
+		if (destPitch)
+			destLine = (D*) ((char*) srcLine + destPitch);
+		else
+			destLine = destByte;
 	}
 }
 
 bmp_handle load_bmp(stdx::data_range_param<const char> imageData, int flags = 0)
 {
+	// Load image header
+	mem_handle fifStream( FreeImage_OpenMemory((BYTE*)(imageData.data()), static_cast<DWORD>(imageData.size())) );
+
+	auto fifType = FreeImage_GetFileTypeFromMemory(fifStream);
+	if (fifType == FIF_UNKNOWN) throwx(img_error("Unknown or unidentifiable file format (FreeImage)"));
+
+	bmp_handle bmp( FreeImage_LoadFromMemory(fifType, fifStream, flags) );
+	if (!bmp.valid()) throwx(img_error("Unsupported image type or format (FreeImage)"));
+
+	return bmp;
+}
+
+FREE_IMAGE_TYPE free_image_type(ImageType::T destElementType)
+{
+	switch (destElementType)
+	{
+	case ImageType::int8bpp: return FIT_UINT8;
+	case ImageType::int16bpp: return FIT_UINT16;
+	case ImageType::float32bpp: return FIT_FLOAT;
+	}
+	return FIT_UNKNOWN;
+}
+
+unsigned bits_per_pixel(ImageType::T destElementType, unsigned destChannels)
+{
+	switch (destElementType)
+	{
+	case ImageType::int8bpp: return destChannels * 8;
+	case ImageType::int16bpp: return destChannels * 16;
+	case ImageType::float32bpp: return destChannels * 32;
+	}
+	return 0;
+}
+
+bmp_handle make_bmp(stdx::data_range_param<const char> imageData, unsigned width, unsigned height, ImageType::T destElementType, unsigned destChannels, int flags = 0)
+{
+	bmp_handle bmp( FreeImage_AllocateT(free_image_type(destElementType), width, height, bits_per_pixel(destElementType, destChannels)) );
+	if (!bmp.valid()) throwx(img_error("Unsupported image type or format (FreeImage)"));
+
+	// copy to FI bmp
+	{
+		auto bytes = FreeImage_GetBits(bmp);
+		auto pitch = FreeImage_GetPitch(bmp);
+		auto srcBytesPerLine = imageData.size() / height;
+
+		if (pitch * height == imageData.size())
+			memcpy(bytes, imageData.data(), imageData.size());
+		else if (srcBytesPerLine < pitch && srcBytesPerLine * height == imageData.size()) {
+			for (auto srcLine = imageData.first; srcLine != imageData.last; srcLine += srcBytesPerLine, bytes += pitch)
+				memcpy(bytes, srcLine, srcBytesPerLine);
+		}
+		else
+			throwx(img_error("Invalid pitch"));
+	}
+
+	return bmp;
+}
+
+bmp_handle save_bmp(stdx::data_range_param<const char> imageData, ImageType::T destElementType, unsigned destChannels, int flags = 0)
+{
+	FreeImageIO
+
 	// Load image header
 	mem_handle fifStream( FreeImage_OpenMemory((BYTE*)(imageData.data()), static_cast<DWORD>(imageData.size())) );
 
@@ -258,6 +326,149 @@ void load_image(void* destImage, ImageType::T destElementType, unsigned destChan
 		throwx(img_error("Unsupported image format"));
 	}
 }
+
+void load_image(void* destImage, ImageType::T destElementType, unsigned destChannels, void* (*allocate)(ImageDesc const& desc, void* image),
+				stdx::data_range_param<const char> imageData)
+{
+	prepareFreeImage();
+
+	// Load image
+	auto bmp = load_bmp(imageData);
+
+	// Read header
+	auto destDesc = image_desc(bmp);
+	unsigned numPixels = destDesc.dim.x * destDesc.dim.y * destDesc.dim.z;
+
+	// Fix up desc
+	destDesc.channels = destChannels;
+
+	// Allocate dest image
+	char* destBytes = (char*) allocate(destDesc, destImage);
+
+	auto colType = FreeImage_GetColorType(bmp);
+	auto imgType = FreeImage_GetImageType(bmp);
+
+	// Convert
+	switch(destElementType)
+	{
+	case ImageType::int8bpp:
+		{
+			// convert to rgb or rgba bitmap
+			if (imgType != FIT_BITMAP || colType != FIC_RGB && colType != FIC_RGBALPHA)
+			{
+				if (imgType == FIT_UINT16 || imgType == FIT_INT16)
+					bmp.reset( FreeImage_ConvertToStandardType(bmp) );
+				else
+					bmp.reset( FreeImage_ConvertTo32Bits(bmp) );
+			}
+
+			// check if potential conversions successful
+			if (!bmp.valid()) throwx(img_error("Unsupported image format conversion"));
+			imgType = FreeImage_GetImageType(bmp);
+			colType = FreeImage_GetColorType(bmp);
+			
+			// may assume FIC_RGB or FIC_RGBA here
+			assert(imgType == FIT_BITMAP && (colType == FIC_RGB || colType == FIC_RGBALPHA));
+
+			auto bytesPerPixel = FreeImage_GetBPP(bmp) / 8;
+			auto channels = bytesPerPixel / 1;
+			auto pitch = FreeImage_GetPitch(bmp);
+			auto bits = FreeImage_GetBits(bmp);
+
+			if (channels == destDesc.channels && pitch == destDesc.dim.x * bytesPerPixel)
+				memcpy(destBytes, bits, numPixels * bytesPerPixel);
+			else
+				copy_channels(
+					reinterpret_cast<unsigned char*>(destBytes), destDesc.channels,
+					reinterpret_cast<unsigned char const*>(bits), channels, pitch,
+					glm::uvec2(destDesc.dim), (unsigned char)(0x00), (unsigned char)(0xff), copy_channel_identity());
+		}
+		break;
+	case ImageType::int16bpp:
+		{
+			// convert to int16
+			if (imgType != FIT_UINT16 && imgType != FIT_INT16 && imgType != FIT_RGB16 && imgType != FIT_RGBA16)
+				bmp.reset( FreeImage_ConvertToRGB16(bmp) );
+
+			// check if potential conversions successful
+			if (!bmp.valid()) throwx(img_error("Unsupported image format conversion"));
+			imgType = FreeImage_GetImageType(bmp);
+			colType = FreeImage_GetColorType(bmp);
+			
+			assert(imgType == FIT_UINT16 || imgType == FIT_INT16 || imgType == FIT_RGB16 || imgType == FIT_RGBA16);
+			
+			auto bytesPerPixel = FreeImage_GetBPP(bmp) / 8;
+			auto channels = bytesPerPixel / 2;
+			auto pitch = FreeImage_GetPitch(bmp);
+			auto bits = FreeImage_GetBits(bmp);
+
+			if (channels == destDesc.channels && pitch == destDesc.dim.x * bytesPerPixel)
+				memcpy(destBytes, bits, numPixels * bytesPerPixel);
+			else
+				copy_channels(
+					reinterpret_cast<unsigned short*>(destBytes), destDesc.channels,
+					reinterpret_cast<unsigned short const*>(bits), channels, pitch,
+					glm::uvec2(destDesc.dim), (unsigned short)(0x0000), (unsigned short)(0xffff), copy_channel_identity());
+		}
+		break;
+	case ImageType::float32bpp:
+		if (imgType == FIT_FLOAT || imgType == FIT_RGBF || imgType == FIT_RGBAF)
+		{
+			auto bytesPerPixel = FreeImage_GetBPP(bmp) / 8;
+			auto channels = bytesPerPixel / 4;
+			auto pitch = FreeImage_GetPitch(bmp);
+			auto bits = FreeImage_GetBits(bmp);
+
+			if (channels == destDesc.channels && pitch == destDesc.dim.x * bytesPerPixel)
+				memcpy(destBytes, bits, numPixels * bytesPerPixel);
+			else
+				copy_channels(
+					reinterpret_cast<float*>(destBytes), destDesc.channels,
+					reinterpret_cast<float const*>(bits), channels, pitch,
+					glm::uvec2(destDesc.dim), 0.0f, 1.0f, copy_channel_identity());
+		}
+		else if (imgType == FIT_BITMAP)
+		{
+			if (colType != FIC_RGB && colType != FIC_RGBALPHA)
+				bmp.reset( FreeImage_ConvertTo32Bits(bmp) );
+
+			// check if potential conversions successful
+			if (!bmp.valid()) throwx(img_error("Unsupported image format conversion"));
+			imgType = FreeImage_GetImageType(bmp);
+			colType = FreeImage_GetColorType(bmp);
+
+			// may assume FIC_RGB or FIC_RGBA here
+			assert(imgType == FIT_BITMAP && (colType == FIC_RGB || colType == FIC_RGBALPHA));
+
+			auto bytesPerPixel = FreeImage_GetBPP(bmp) / 8;
+			auto channels = bytesPerPixel / 1;
+			auto pitch = FreeImage_GetPitch(bmp);
+			auto bits = FreeImage_GetBits(bmp);
+
+			copy_channels(
+				reinterpret_cast<float*>(destBytes), destDesc.channels,
+				reinterpret_cast<unsigned char const*>(bits), channels, pitch,
+				glm::uvec2(destDesc.dim), 0.0f, 1.0f, [](unsigned char val){ return float(val) / float(0xff); });
+		}
+		else if (imgType == FIT_UINT16 || imgType == FIT_INT16 || imgType == FIT_RGB16 || imgType == FIT_RGBA16)
+		{
+			auto bytesPerPixel = FreeImage_GetBPP(bmp) / 8;
+			auto channels = bytesPerPixel / 2;
+			auto pitch = FreeImage_GetPitch(bmp);
+			auto bits = FreeImage_GetBits(bmp);
+
+			copy_channels(
+				reinterpret_cast<float*>(destBytes), destDesc.channels,
+				reinterpret_cast<unsigned short const*>(bits), channels, pitch,
+				glm::uvec2(destDesc.dim), 0.0f, 1.0f, [](unsigned short val){ return float(val) / float(0xffff); });
+		}
+		break;
+
+	default:
+		throwx(img_error("Unsupported image format"));
+	}
+}
+
 
 namespace
 {
